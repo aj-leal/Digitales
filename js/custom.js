ba249eb48e9f1fdc95211b00d74af42ee7ba264b
aj-leal <adrian.leal23@gmail.com>
2024-06-07 18:07:50 +0800

    Added question components with choices and hides the intro components

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,16 @@
 function setUpPages(){
 	//for(let i=2; i<8; i++)let
-	let i=2;
-	document.getElementById("page"+i).style.display = "none";
+	document.getElementById("page2").style.display = "none";
+	document.getElementById("page3").style.display = "none";
 }
 
+let name = "", gradeAndSection="";
+function goToQuestionPage(){
+	name = document.getElementById("fname").value;
+	gradeAndSection = document.getElementById("gradeAndSection").value;
+
+	//console.log(name+"  "+gradeAndSection);
+
+	document.getElementById("page1").style.display = "none";
+	document.getElementById("page2").style.display = "block";
+}
